0fb81a3cf0233f4fad34168744c06e770aa060f6
Matias <matiasnoriega86@gmail.com>
2021-05-12 21:24:57 -0300

    (reformat): usa filtro nuevo a pedido del desarrollador del plugin

--- feature_image_remover.php
+++ feature_image_remover.php
@@ -1,34 +1,34 @@
 <?php
-   /*
-   Plugin Name: Featured Image Remover
-   Plugin URI: http://github.com/matiasnoriega/featured_image_remover
-   description: >-
-  plugin para dejar de mostrar la imagen destacada de AMP a hasta determinada fecha
-   Version: 1.0
-   Author: Matias Nicolas Noriega
-   Author URI: http://github.com/matiasnoriega/
-   License: GPL3
-   */
 
+/*
+Plugin Name: Featured Image Remover
+Plugin URI: http://github.com/matiasnoriega/featured_image_remover
+description: >-
+plugin para dejar de mostrar la imagen destacada de AMP a hasta determinada fecha
+Version: 1.0
+Author: Matias Nicolas Noriega
+Author URI: http://github.com/matiasnoriega/
+License: GPL3
+*/
 
-add_filter( 
-	'amp_post_template_data', 
-	function ( $data ) {
+add_filter('ampforwp_modify_featured_image','ampforwp_featured_image_date');
 
-		$post = get_post();
+/**
+ * @param $image
+ * @return string
+ */
 
-		/* Fecha del Post en formato Time */
-		$post_date = strtotime($post->post_date);
-		
-		/* Fecha límite (en formato YYYY-MM-DD) que se pasa a formato Time */
-		$date = strtotime("2021-02-10");
+function ampforwp_featured_image_date($image){
+	# Fecha en formato YYYY-MM-DD
+	$fecha = "2021-05-13";
+	
+	if (get_post_time() < strtotime($fecha)){
+	
+		$image = '';
 
-		if($post_date < $date){
+	}
+	
+	return $image;
+}
 
-			$data['featured_image'] = false;
-
-		}
-		return $data;
-	} 
-);
 ?>
